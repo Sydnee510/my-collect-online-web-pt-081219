f183b799e6be6bb0248765dac97ec430db13dab2
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-22 02:33:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+i = 0 
+while i < array.length 
+  yield(array[i])
+  i = i + 1
+end
+my_collect(languages) do |language|
+         language.upcase
+end
+my_collect(students) do |student|
+         student.split(" ").first
+end
+end 
